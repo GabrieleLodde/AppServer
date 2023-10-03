8017b3dcaf527beb0c2d9936883d3a98013ff281
lai2-01 <lai2-01@itismeucci.com>
2023-10-03 09:06:29 +0200

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try{
+            System.out.println("Server avviato...");
+            ServerSocket server = new ServerSocket(3000);
+
+            Socket s = server.accept();
+            System.out.println("Un client si è connesso");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            String stringaRicevuta = in.readLine();
+            System.out.println("Il client ha inviato " + stringaRicevuta);
+
+            String stringaModificata = stringaRicevuta.toUpperCase();
+            out.writeBytes(stringaModificata + "\n");
+
+            server.close();
+            s.close();
+
+        } catch( Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'istanza del server");
+            System.exit(1);
+        }
+    }
+}
